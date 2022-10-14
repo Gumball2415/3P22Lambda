fca45a91bec6c36982027b6e6d108b45a16e2a67
Persune <gumball2415@gmail.com>
2022-10-14 19:23:57 +0800

    Add LodePNG

--- source/main.cpp
+++ source/main.cpp
@@ -1,14 +1,29 @@
 ﻿#include "main.h"
 #include "config.h"
+#include "lodepng/lodepng.h"
 
-int main()
+int main(int argc, char* argv[])
 {
 	std::cout << PROJECT_NAME <<" v."
 		<< PROJECT_VERSION << std::endl
 		<< PROJECT_DESCRIPTION << std::endl;
+	std::cout << "uses LodePNG, Copyright (c) 2005-2010 Lode Vandevenne" << std::endl;
+
+	// TODO: handle arguments
 
 	// pause
 	std::cout << "Press enter to continue . . . ";
 	std::cin.get();
 	return 0;
+
+	// step 1: break down video into .png frames
+	// step 2: process each frame through ProcessFrame()
+	// step 3: reencode processed frames back into video
+	// step 4: profit
+
+}
+
+void ProcessFrame()
+{
+	return;
 }
