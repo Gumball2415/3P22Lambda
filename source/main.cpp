7da8a6aa7d73da7dfdf74fb48fc5be67fe17f6a5
Persune <gumball2415@gmail.com>
2023-02-04 22:58:45 +0800

    Add TCLAP for commandline argument parsing

--- source/main.cpp
+++ source/main.cpp
@@ -1,26 +1,57 @@
-﻿#include "main.h"
+﻿#include <string>
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+#include "main.h"
 #include "config.h"
 #include "lodepng/lodepng.h"
+#include <tclap/CmdLine.h>
 
 int main(int argc, char* argv[])
 {
-	std::cout << PROJECT_NAME <<" v."
-		<< PROJECT_VERSION << std::endl
-		<< PROJECT_DESCRIPTION << std::endl;
-	std::cout << "uses LodePNG, Copyright (c) 2005-2010 Lode Vandevenne" << std::endl;
+	std::cout << PROJECT_NAME << std::endl
+		<< PROJECT_DESCRIPTION << std::endl
+		<< "uses LodePNG, Copyright (c) 2005-2010 Lode Vandevenne" << std::endl;
 
-	// TODO: handle arguments
+	try {
+		TCLAP::CmdLine cmd("uses TCLAP, Copyright (c) 2017-2021 Google LLC, (c) 2012 - 2016 Daniel Aarno, (c) 2003 - 2012 Michael E.Smoot", ' ', PROJECT_VERSION);
+		TCLAP::ValueArg<std::string> inpath_arg(
+			"i",
+			"input",
+			"input path to folder of .png sequence",
+			true,
+			"",
+			"path");
+		TCLAP::ValueArg<std::string> outpath_arg(
+			"o",
+			"output",
+			"output path to folder of .png sequence",
+			true,
+			"",
+			"path");
 
-	// pause
-	std::cout << "Press enter to continue . . . ";
-	std::cin.get();
-	return 0;
+		cmd.add(inpath_arg);
+		cmd.add(outpath_arg);
 
-	// step 1: break down video into .png frames
+		cmd.parse(argc, argv);
+
+		std::string inpath = inpath_arg.getValue();
+		std::string outpath = outpath_arg.getValue();
+
+		std::cout << inpath << std::endl;
+		std::cout << outpath << std::endl;
+
+	}
+	catch (TCLAP::ArgException& e)
+	{
+		std::cerr << "error: " << e.error() << " for arg " << e.argId() << std::endl;
+	}
+
+	// step 1: count input .png frames
 	// step 2: process each frame through ProcessFrame()
 	// step 3: reencode processed frames back into video
 	// step 4: profit
-
+	return 0;
 }
 
 void ProcessFrame()
